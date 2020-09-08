e723bceb8b2aa0215a326ca511c54d04109a1752
Blake-McCullough <66660752+Blake-McCullough@users.noreply.github.com>
2020-09-08 12:55:18 +0800

    Add files via upload

---
+++ name to gmail.py
@@ -0,0 +1,13 @@
+print("Made by Blake McCullough") #prints creators name
+continu = 2
+print("to stop the code type stop with no capitals")
+while continu == 2:
+    end="@gmail.com"
+    email=input("What is the email u want converted")
+    if email == "stop":
+        quit()
+    else:
+        email=email+end
+        print(email)
+    
+
